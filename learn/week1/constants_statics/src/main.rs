3a354742783ae8566ffe9dd2d0067093c6cbd0e9
Jonathan Lin <jlin@winteroftext.ca>
2023-11-13 22:44:27 -0800

    declare some const and static variables

--- learn/week1/constants_statics/src/main.rs
+++ learn/week1/constants_statics/src/main.rs
@@ -1,3 +1,12 @@
+// constants must have explicit type annotation
+// global scope :D
+const MAX_COUNT: u8 = 10;
+
+
+// static variables must have explicit type annotation
+// accessing or modifying mutatable static is unsafe
+static GAME_NAME: &str = "Draw Five";
+
 fn main() {
     println!("Hello, world!");
 

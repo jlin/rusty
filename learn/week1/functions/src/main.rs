080dca50f96dbb13bbe8483588b6f15a575fef13
Jonathan Lin <jlin@winteroftext.ca>
2023-11-14 20:54:47 -0800

    functions lesson

--- learn/week1/functions/src/main.rs
+++ learn/week1/functions/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
     println!("Hello, world!");
+    let z = my_function(15);
+    println!("my_function returned: {}", z)
+}
+
+fn my_function(x: i32) -> i32 {
+    // return 5; // early return possible with the return keyword
+    println!("my_function called with: {}", x);
+    let y = 10;
+    y // special no semicolon syntax at the last expression returns this last variable
+    // this can be an expression
 }

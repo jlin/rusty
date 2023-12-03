9a6d21de50f29200152131d93ff41c58e32a448a
Jonathan Lin <jlin@winteroftext.ca>
2023-12-02 23:57:34 -0800

    implementation block, methods, new creator and associated functions

--- learn/week3/structs/src/main.rs
+++ learn/week3/structs/src/main.rs
@@ -4,6 +4,43 @@ struct Product {
     in_stock: bool
 }
 
+impl Product{
+    fn new(name: String, price: f32) -> Product {
+        Product{
+            // redundant to set name to name, just give it name
+            //name: name,
+            name,
+            
+            // redundant to set price to price, just give it price
+            //price: price,
+            price,
+            in_stock: true,
+        
+        }
+    }
+    fn calculate_sales_tax(&self) -> f32 {
+        self.price * Product::get_default_sales_tax()
+        // use the :: syntax on the struct name to call associated functions.
+    }
+
+     // associated function doesn't take self (of any kind) as an input/parameter
+     fn get_default_sales_tax() -> f32 {
+        0.1
+    }
+    
+    fn set_price(&mut self, price:f32) {
+        self.price = price
+    } 
+
+    fn buy(self) -> i32 {
+        let name = self.name;
+        println!("{name} was bought!");
+        12345
+    }
+
+   
+}
+
 fn main() {
     println!("Hello, world!");
     
@@ -15,10 +52,18 @@ fn main() {
     let price = book.price;
     book.in_stock=false;
 
-    let sales_tax = calculate_sales_tax(&book);
-    println!("{sales_tax}")
+    let sales_tax = book.calculate_sales_tax();
+    println!("{sales_tax}");
+
+    book.set_price(1.00);
+    book.buy();
+
+    // not allowed to set book price because the buy method owns the book. since the method ended
+    // the variable is dropped, and so is no longer available to change
+
+    // book.set_price(5.00);
+
+    let mut book = Product::new(String::from("newbook2"), 5000.00);
+
 }
 
-fn calculate_sales_tax(product: &Product) -> f32 {
-    product.price * 0.10
-}
